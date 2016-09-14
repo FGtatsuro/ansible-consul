b7db927f4ecef75b01cb010cb747a7a1cb372234
FGtatsuro <fujiistorage@gmail.com>
2016-09-15 01:36:53 +0900

    Add specs for consul config.

--- spec/consul_spec.rb
+++ spec/consul_spec.rb
@@ -9,5 +9,17 @@ describe command('consul version'), :if => ['alpine', 'debian'].include?(os[:fam
 end
 
 describe file('/usr/local/bin/consul') do
-    it { should be_executable }
+  it { should be_executable }
+end
+
+[
+  '/etc/consul.d/web.json',
+  '/etc/consul.d/db.json'
+].each do |f|
+  describe file(f) do
+    it { should be_file }
+    it { should be_readable }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+  end
 end
