8927cb68cd8300eac8752d9391d2101988f49838
FGtatsuro <fujiistorage@gmail.com>
2017-02-17 10:19:24 +0900

    Add specs related to run/daemon scripts.

--- spec/consul_spec.rb
+++ spec/consul_spec.rb
@@ -12,6 +12,43 @@ describe file('/usr/local/bin/consul') do
   it { should be_executable }
 end
 
+# Default settings
+[
+  '/opt/consul/daemon.py',
+  '/opt/consul/services.sh'
+].each do |f|
+  describe file(f) do
+    it { should be_mode 755 }
+    it { should be_owned_by ENV['CONSUL_CONFIG_OWNER'] }
+    it { should be_grouped_into ENV['CONSUL_CONFIG_GROUP'] }
+  end
+end
+describe file('/opt/consul/daemon.py') do
+  its(:content) { should match /\/opt\/consul\/services\.sh/ }
+  its(:content) { should match /\/var\/log\/consul\/stdout\.log/ }
+  its(:content) { should match /\/var\/log\/consul\/stderr\.log/ }
+  its(:content) { should match /\/var\/lock\/consul\.pid/ }
+  it { should be_executable }
+  it { should be_owned_by ENV['CONSUL_CONFIG_OWNER'] }
+  it { should be_grouped_into ENV['CONSUL_CONFIG_GROUP'] }
+end
+
+describe package('python-daemon') do
+  it { should be_installed.by(:pip) }
+end
+
+[
+  '/var/log/consul',
+  '/var/lock'
+].each do |f|
+  describe file(f) do
+    it { should exist }
+    it { should be_owned_by ENV['CONSUL_CONFIG_OWNER'] }
+    it { should be_grouped_into ENV['CONSUL_CONFIG_GROUP'] }
+  end
+end
+
+# Custom settings
 [
   "#{ENV['CONSUL_CONFIG_REMOTE_DIR']}/web.json",
   "#{ENV['CONSUL_CONFIG_REMOTE_DIR']}/db.json"
