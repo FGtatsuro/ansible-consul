5c93f5bf9b08c74b0acd08287dabac7d164db9f8
FGtatsuro <fujiistorage@gmail.com>
2017-02-24 15:59:36 +0900

    Refactor daemon script with subcommands.

--- templates/daemons.py
+++ templates/daemons.py
@@ -6,6 +6,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import click
 import os
 import signal
 import subprocess
@@ -18,9 +19,12 @@ stdout = open('{{ consul_daemon_log_dir }}/stdout.log', mode='a+')
 stderr = open('{{ consul_daemon_log_dir }}/stderr.log', mode='a+')
 pidfile = PIDLockFile('{{ consul_daemon_pid_dir }}/consul.pid')
 
+context_settings = dict(help_option_names=['-h', '--help'])
 
-def do_start(commands):
-    with daemon.DaemonContext(stdout=stdout, stderr=stderr, pidfile=pidfile):
+
+def do_start(mode):
+    commands = ['{{ consul_script_remote_dir }}/services.sh', mode]
+    with daemon.DaemonContext(stdout=stdout, stderr=stderr, pidfile=pidfile) as context:
         child_process = subprocess.Popen(commands)
         child_process.communicate()
         child_process.terminate()
@@ -30,17 +34,39 @@ def do_stop():
     os.killpg(os.getpgid(pidfile.read_pid()), signal.SIGTERM)
 
 
-def do_restart():
+@click.group(context_settings=context_settings)
+def consul_cli():
+    '''
+    CLI to manage Consul daemon processes.
+    '''
+    pass
+
+
+@consul_cli.command()
+@click.argument('mode', type=click.Choice(['dev', 'client', 'server']))
+def start(mode):
+    '''
+    Start Consul daemon processes on MODE(dev/client/server).
+    '''
+    do_start(mode)
+
+
+@consul_cli.command()
+def stop():
+    '''
+    Stop Consul daemon processes.
+    '''
     do_stop()
-    do_start()
+
+
+@consul_cli.command()
+@click.argument('mode', type=click.Choice(['dev', 'client', 'server']))
+def restart(mode):
+    '''
+    Restart Consul daemon processes on MODE(dev/client/server).
+    '''
+    do_stop()
+    do_start(mode)
 
 if __name__ == '__main__':
-    base_command = '{{ consul_script_remote_dir }}/services.sh'
-    if sys.argv[1] == 'start':
-        do_start([base_command] + sys.argv[2:])
-    elif sys.argv[1] == 'stop':
-        do_stop()
-    elif sys.argv[1] == 'restart':
-        do_restart()
-    else:
-        do_start([base_command] + sys.argv[2:])
+    consul_cli()
