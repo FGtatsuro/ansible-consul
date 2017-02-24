eea2ad51f5387b30b5b559bae1d129d1d4e415d9
FGtatsuro <fujiistorage@gmail.com>
2017-02-24 10:25:19 +0900

    Support start/stop/restart of daemon.

--- templates/daemons.py
+++ templates/daemons.py
@@ -6,15 +6,41 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+import signal
 import subprocess
 import sys
 
 import daemon
-import lockfile.pidlockfile
+from lockfile.pidlockfile import PIDLockFile
+
+stdout = open('{{ consul_daemon_log_dir }}/stdout.log', mode='a+')
+stderr = open('{{ consul_daemon_log_dir }}/stderr.log', mode='a+')
+pidfile = PIDLockFile('{{ consul_daemon_pid_dir }}/consul.pid')
+
+
+def do_start(commands):
+    with daemon.DaemonContext(stdout=stdout, stderr=stderr, pidfile=pidfile):
+        child_process = subprocess.Popen(commands)
+        child_process.communicate()
+        child_process.terminate()
+
+
+def do_stop():
+    os.killpg(os.getpgid(pidfile.read_pid()), signal.SIGTERM)
+
+
+def do_restart():
+    do_stop()
+    do_start()
 
 if __name__ == '__main__':
-    stdout = open('{{ consul_daemon_log_dir }}/stdout.log', mode='a+')
-    stderr = open('{{ consul_daemon_log_dir }}/stderr.log', mode='a+')
-    pidfile = lockfile.pidlockfile.PIDLockFile('{{ consul_daemon_pid_dir }}/consul.pid')
-    with daemon.DaemonContext(stdout=stdout, stderr=stderr, pidfile=pidfile):
-        subprocess.call(['{{ consul_script_remote_dir }}/services.sh'] + sys.argv[1:])
+    base_command = '{{ consul_script_remote_dir }}/services.sh'
+    if sys.argv[1] == 'start':
+        do_start([base_command] + sys.argv[2:])
+    elif sys.argv[1] == 'stop':
+        do_stop()
+    elif sys.argv[1] == 'restart':
+        do_restart()
+    else:
+        do_start([base_command] + sys.argv[2:])
